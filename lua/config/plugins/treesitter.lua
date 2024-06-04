348e066bee90afdc1328e3674d32b98d4bf61a84
lijiang99 <bd7xzz@163.com>
2024-06-05 02:55:29 +0800

    enable treesitter

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -1,16 +1,16 @@
-return {
-	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"c", "cpp", "cmake", "latex",
-				"lua", "python", "bash",
-				"vim", "vimdoc", "query",
-			},
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end
-}
+-- return {
+-- 	"nvim-treesitter/nvim-treesitter",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		require("nvim-treesitter.configs").setup({
+-- 			ensure_installed = {
+-- 				"c", "cpp", "cmake", "latex",
+-- 				"lua", "python", "bash",
+-- 				"vim", "vimdoc", "query",
+-- 			},
+-- 			highlight = { enable = true },
+-- 			indent = { enable = true },
+-- 		})
+-- 	end
+-- }
